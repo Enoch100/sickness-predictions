dd0b5df14b8b6c1e333723182a17dc40fb23d54d
Enoch100 <80264232+Enoch100@users.noreply.github.com>
2025-11-06 22:42:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ URL = "https://drive.google.com/uc?id=1645wfzc0VQ_lXzcOrXKhgvWM8EsdZcYf"
 #URL = "https://drive.google.com/uc?id=1zPNJbJhH5SeKFB-JaKoA3DIkFeDBHUbA"
 r=requests.get(URL)
 output = "AAsymptom_checker.pkl"
-output = "ssymptom_checker.pkl"
+#output = "ssymptom_checker.pkl"
 st.write("Downloading the model from google drive...")
 gdown.download(URL, output, quiet=False)
 with open(output, "rb") as f:
